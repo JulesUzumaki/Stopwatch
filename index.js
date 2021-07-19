9a495b2a50ac28c4ed0dd3de5d5525e01f51041c
julie <cabanat.julie@eleves.lfiduras.com>
2021-07-19 08:42:40 +0700

    test1

---
+++ index.js
@@ -0,0 +1,8 @@
+let seconds = 00;
+let msseconds = 00;
+
+let appendSeconds =  document.getElementById('seconds');
+let appendMsSeconds = document.getElementById('msseconds');
+let reset = document.getElementById('reset');
+let start = document.getElementById('start');
+let stop = document.getElementById('stop');
